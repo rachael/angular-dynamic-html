cadf492ce6d01e93c24df9521fdf488ee5ad8912
Rachael Passov <github@rachael.io>
2016-06-20 11:04:40 -0400

    fixed 'cannot read property insertBefore of null' ng-repeat bug in demo

--- demo/src/demo.js
+++ demo/src/demo.js
@@ -33,22 +33,27 @@ var app = angular
 
     $scope.needsBroadcastHtmlExample = '<div dynamic-html="{{generateDynamicHtml(displayedCollection)}}"></div>';
     $scope.needsBroadcastControllerExample =
-      `$scope.eatSleepCodeRepeat = [ {id: 2, text: "code"} ... ];\
-        \n$scope.eatSleepHackRepeat = [ {id: 2, text: "hack"} ... ];\
+      `$scope.eatSleepCodeRepeat = [ ... {id: 2, text: "code"} ... ];\
+        \n$scope.eatSleepHackRepeat = [ ... {id: 2, text: "hack"} ... ];\
         \n$scope.displayedCollection = "eatSleepCodeRepeat";\
         \n$scope.order = "id";\
         \n\n$scope.generateDynamicHtml = function(collectionToDisplay) {\
         \n\tlet _html =\
-        \n\t\t\`<p ng-repeat="item in \$\{collectionToDisplay\} | orderBy:order">\\\
-        \n\t\t\t{{item.id + 1}}. {{item.text}}\\\
-        \n\t\t</p>\`;\
+        \n\t\t\`<div>\
+        \n\t\t\t<p ng-repeat="item in \$\{collectionToDisplay\} | orderBy:order">\\\
+        \n\t\t\t\t{{item.id + 1}}. {{item.text}}\\\
+        \n\t\t\t</p>\
+        \n\t\t</div>\`;\
         \n\treturn $sce.trustAsHtml(_html);\
       \n}`;
+
     $scope.generateDynamicHtml = function(collectionToDisplay) {
       let _html =
-        `<p ng-repeat="item in ${collectionToDisplay} | orderBy:order">\
-          {{item.id + 1}}. {{item.text}}\
-        </p>`;
+        `<div>\
+          <p ng-repeat="item in ${collectionToDisplay} | orderBy:order">\
+            {{item.id + 1}}. {{item.text}}\
+          </p>\
+        </div>`;
       return $sce.trustAsHtml(_html);
     };
 
